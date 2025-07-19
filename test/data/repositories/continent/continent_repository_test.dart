1e2cf6c0415b24b94676272bf23fad365dc2dc5e
sb-dor <avaz.shams.2002@gmail.com>
2025-07-19 18:09:52 +0500

    added continent repository local test which closes #13

--- test/data/repositories/continent/continent_repository_test.dart
+++ test/data/repositories/continent/continent_repository_test.dart
@@ -62,4 +62,38 @@ void main() {
     });
   });
 
+
+  group('Local test', () {
+    //
+    test('Test getContinents for success', () async {
+      // arrange
+      when(internetConnectionCheckerHelper.hasAccessToInternet()).thenAnswer((_) async => false);
+      when(continentLocalService.getContinents()).thenAnswer((_) async => fakeContinents);
+
+      // act
+      final getContinents = await continentRepository.getContinents();
+
+      // assert
+      expect(getContinents, isNotEmpty);
+      verify(internetConnectionCheckerHelper.hasAccessToInternet()).called(1);
+      verify(continentLocalService.getContinents()).called(1);
+    });
+
+    //
+    test('Test getContinents for failure', () async {
+      // arrange
+      when(internetConnectionCheckerHelper.hasAccessToInternet()).thenAnswer((_) async => false);
+      when(
+        continentLocalService.getContinents(),
+      ).thenThrow(Exception()); // if you have your own written exception use them
+
+      // act
+      // do not use await cause you service will throw an exception
+      final getContinents = continentRepository.getContinents();
+
+      // assert
+      expect(getContinents, throwsException);
+      verify(internetConnectionCheckerHelper.hasAccessToInternet()).called(1);
+    });
+  });
 }

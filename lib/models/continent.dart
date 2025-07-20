413287727efc66b8243c8326c342985b5ade8427
sb-dor <avaz.shams.2002@gmail.com>
2025-07-20 08:27:42 +0500

    added test for Continent remote service which closes #19

--- lib/models/continent.dart
+++ lib/models/continent.dart
@@ -5,9 +5,10 @@ class Continent {
   final String imageUrl;
 
   factory Continent.fromJson(Map<String, dynamic> json) {
-    return Continent(
-      name: json['name'] as String,
-      imageUrl: json['imageUrl'] as String,
-    );
+    return Continent(name: json['name'] as String, imageUrl: json['imageUrl'] as String);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'name': name, 'imageUrl': imageUrl};
   }
 }
---
+++ test/data/services/continent/continent_remote_service_test.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+import 'package:architectures/data/services/continent/continent_remote_service.dart';
+import 'package:architectures/data/services/continent/continent_service.dart';
+import 'package:architectures/models/continent.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import "package:http/testing.dart" as http_testing;
+
+import '../../../../testing_data/temp_data/fake_continents.dart';
+
+void main() {
+  final String mainUrl = "";
+  final Map<String, dynamic> success = {"success": true};
+  final Map<String, dynamic> failure = {"success": false};
+  final String internalServerError = "Internal Server Error";
+
+  group('Continent Remote Service Test', () {
+    //
+    test('Test getContinents for success', () async {
+      //
+      final mockedClient = http_testing.MockClient(
+        (_) async => http.Response(
+          jsonEncode(fakeContinents.map((element) => element.toJson()).toList()),
+          200,
+        ),
+      );
+
+      final IContinentService continentService = ContinentRemoteService(
+        mainUrl: mainUrl,
+        client: mockedClient,
+      );
+
+      final continents = await continentService.getContinents();
+
+      expect(continents, isNotEmpty);
+      mockedClient.close();
+    });
+
+    //
+    test('Test getContinents for failure', () async {
+      //
+      final mockedClient = http_testing.MockClient(
+        (_) async => http.Response(internalServerError, 500),
+      );
+
+      final IContinentService continentService = ContinentRemoteService(
+        mainUrl: mainUrl,
+        client: mockedClient,
+      );
+
+      final continents = await continentService.getContinents();
+
+      expect(continents, isEmpty);
+      mockedClient.close();
+    });
+  });
+}

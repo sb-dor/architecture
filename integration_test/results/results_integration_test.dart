f5e0154280137e2c9f74040bf5b5353c96951626
sb-dor <avaz.shams.2002@gmail.com>
2025-08-06 23:55:02 +0500

    writing test for results integration test

---
+++ integration_test/results/results_integration_test.dart
@@ -0,0 +1,8 @@
+import 'package:architectures/data/services/destination/destination_service.dart';
+import 'package:architectures/data/services/itinerary_config_service/itinerary_config_service.dart';
+import 'package:architectures/utils/internet_connection_checker_helper.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([IDestinationService, IItineraryConfigService, InternetConnectionCheckerHelper])
+void main() {
+}

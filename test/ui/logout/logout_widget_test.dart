1e70a8b7c5c28b10f6cc094e045674b8441f5c57
sb-dor <avaz.shams.2002@gmail.com>
2025-08-09 07:52:43 +0500

    added widget test for logout feature which closes #2

---
+++ test/ui/logout/logout_widget_test.dart
@@ -0,0 +1,75 @@
+import 'package:architectures/data/repositories/auth/auth_repository.dart';
+import 'package:architectures/data/services/auth/auth_service.dart';
+import 'package:architectures/runner/models/dependency_container.dart';
+import 'package:architectures/ui/logout/controllers/logout_controller.dart';
+import 'package:architectures/ui/logout/widgets/logout_button_widget.dart';
+import 'package:architectures/utils/internet_connection_checker_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/test_widget_controller.dart';
+import 'logout_widget_test.mocks.dart';
+
+@GenerateMocks([IAuthService, InternetConnectionCheckerHelper])
+void main() {
+  late final MockIAuthService mockIAuthRemoteService;
+  late final MockIAuthService mockIAuthLocalService;
+
+  late final MockInternetConnectionCheckerHelper mockInternetConnectionCheckerHelper;
+
+  late LogoutController logoutController;
+
+  setUpAll(() {
+    mockIAuthRemoteService = MockIAuthService();
+    mockIAuthLocalService = MockIAuthService();
+
+    mockInternetConnectionCheckerHelper = MockInternetConnectionCheckerHelper();
+
+    final IAuthRepository authRepository = AuthRepositoryImpl(
+      authRemoteService: mockIAuthRemoteService,
+      authLocalService: mockIAuthLocalService,
+      internetConnectionCheckerHelper: mockInternetConnectionCheckerHelper,
+    );
+
+    logoutController = LogoutController(authRepository: authRepository);
+  });
+
+  tearDownAll(() => logoutController.dispose());
+
+  group('Logout integration test', () {
+    //
+
+    testWidgets("Logout widget test for success", (tester) async {
+      //
+      when(mockInternetConnectionCheckerHelper.hasAccessToInternet()).thenAnswer((_) async => true);
+
+      when(mockIAuthRemoteService.logout()).thenAnswer((_) async => true);
+
+      await TestWidgetController(tester).pumpWidget(
+        Scaffold(body: Center(child: LogoutButton())),
+        dependencies: TestLogoutDependencyContainer(logoutController: logoutController),
+      );
+
+      final findButton = find.byKey(ValueKey<String>('logout_button'));
+
+      expect(findButton, findsOneWidget);
+
+      await tester.tap(findButton);
+
+      verify(mockInternetConnectionCheckerHelper.hasAccessToInternet()).called(1);
+      verify(mockIAuthRemoteService.logout()).called(1);
+    });
+  });
+}
+
+final class TestLogoutDependencyContainer extends TestDependencyContainer {
+  TestLogoutDependencyContainer({required LogoutController logoutController})
+    : _logoutController = logoutController;
+
+  final LogoutController _logoutController;
+
+  @override
+  LogoutController get logoutController => _logoutController;
+}
